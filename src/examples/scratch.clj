540f3fe18dbb254c506f38c179ee4911a9ef9179
Ryo TANAKA <ryotan@ucb-jp.net>
2014-04-06 03:19:15 +0900

    seq使ってみた？

--- src/examples/scratch.clj
+++ src/examples/scratch.clj
@@ -10,4 +10,16 @@
 
 (join \, (take 10 (whole-numbers)))
 
-(join \, (take 10 (whole-numbers)))
+(apply str (take 10 (interpose \, (whole-numbers))))
+
+(take 10 (filter even? (whole-numbers)))
+
+(filter even? (take 10 (whole-numbers)))
+
+(take-while (complement #{\a \e \i \o \u}) "the-quick-brown-fox")
+
+(drop-while (complement #{\a \e \i \o \u}) "the-quick-brown-fox")
+
+(split-at 5 (range 10))
+
+(split-with #(<= % 10) (range 0 20 2))
