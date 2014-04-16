52fa382d74a6fef9c0f1cbcd1e3b4fc58b39f452
Ryo TANAKA <ryotan@ucb-jp.net>
2014-04-16 22:45:27 +0900

    Mutal Recursion.

--- src/examples/mutual_recursion.clj
+++ src/examples/mutual_recursion.clj
@@ -1,3 +1,4 @@
+
 (declare my-odd? my-even?)
 
 (defn my-odd? [n]
@@ -52,7 +53,37 @@
     true
     #(my-odd? (dec n))))
 
-(trampoline my-even? 1000000)
+;(trampoline my-even? 1000000)
+
+(declare replace-symbol replace-symbol-expression)
+
+(defn replace-symbol [coll oldsym newsym]
+  (if (empty? coll)
+    ()
+    (cons (replace-symbol-expression
+           (first coll) oldsym newsym)
+          (replace-symbol
+           (rest coll) oldsym newsym))))
+(defn replace-symbol-expression [symbol-expr oldsym newsym]
+  (if (symbol? symbol-expr)
+    (if (= symbol-expr oldsym)
+      newsym
+      symbol-expr)
+    (replace-symbol symbol-expr oldsym newsym)))
+
+(defn deeply-nested [n]
+  (loop [n n
+         result '(bottom)]
+    (if (= n 0)
+      result
+      (recur (dec n) (list result)))))
+
+(deeply-nested 1000)
+
+(replace-symbol (deeply-nested 10) 'bottom 'deepest)
+(replace-symbol (deeply-nested 1000) 'bottom 'deepest)
+
+
 
 
 
