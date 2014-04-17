ca6000b7a7dd76caf4aca95068e51f5892cc2ad7
Ryo TANAKA <ryotan@ucb-jp.net>
2014-04-17 22:43:00 +0900

    Memoization.

---
+++ src/examples/memoization.clj
@@ -0,0 +1,28 @@
+(declare m f)
+
+(defn m [n]
+  (if (zero? n)
+    0
+    (- n (f (m (dec n))))))
+
+(defn f [n]
+  (if (zero? n)
+    1
+    (- n (m (f (dec n))))))
+
+;遅すぎてやばい。
+;(f 250)
+
+
+(def m (memoize m))
+(def f (memoize f))
+
+(m 250)
+
+(f 250)
+
+(def m-seq (map m (iterate inc 0)))
+(def f-seq (map f (iterate inc 0)))
+
+(nth m-seq 100000)
+
