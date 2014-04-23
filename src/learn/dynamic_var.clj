b101d525de7fcdce57a3ca1c395aa1b76c14ca25
Ryo TANAKA <ryotan@ucb-jp.net>
2014-04-23 22:42:24 +0900

    Binding dynamic var.

--- src/learn/dynamic_var.clj
+++ src/learn/dynamic_var.clj
@@ -18,3 +18,23 @@
 (let [*foo* "let foo"] (print-foo))
 ;; bindingだと、print-fooが参照する*foo*に束縛される値は、bindされた値になる。
 (binding [*foo* "let foo"] (print-foo))
+
+
+
+;; おっそい関数
+(defn ^:dynamic slow-double [n]
+  (Thread/sleep 100)
+  (* n 2))
+(defn calls-slow-double []
+  (map slow-double [1 2 1 2 1 2 1 2]))
+;; かなり遅いよ。
+(time (dorun (calls-slow-double)))
+
+;; メモ化する。
+(defn demo-memoize []
+  ;; メモ化した関数をslow-doubleにbindし直す。
+  (binding [slow-double (memoize slow-double)]
+    ;; calls-slow-doubleでは、bindされているメモ化された関数を参照することになる。キm ry)
+    (calls-slow-double)))
+;; 2回しか計算されないから、それなりに早い。
+(time (dorun (demo-memoize)))
