18eb2afc09d7f32e64e8bbf9cc11631d0ff1c5ab
Ryo TANAKA <ryotan@ucb-jp.net>
2014-04-27 22:55:30 +0900

    Protocol.

---
+++ src/learn/gulp.clj
@@ -0,0 +1,15 @@
+(ns learn.gulp
+  (:import (java.io FileInputStream InputStreamReader BufferedReader)))
+
+(defn gulp [src]
+  (let [sb (StringBuilder.)]
+    (with-open [reader (-> src
+                           FileInputStream.
+                           InputStreamReader.
+                           BufferedReader.)]
+      (loop [c (.read reader)]
+        (if (neg? c)
+          (str sb)
+          (do
+            (.append sb c)
+            (recur (.read reader))))))))
